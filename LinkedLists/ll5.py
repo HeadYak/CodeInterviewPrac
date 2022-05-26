f6b2ccd664f798b9186750a1a865a1e6617f58ec
Frank Su <frank.su@student.unsw.edu.au>
2022-05-27 01:37:55 +1000

    reverse sum of linkedlist as numbers done

--- LinkedLists/ll5.py
+++ LinkedLists/ll5.py
@@ -1,6 +1,55 @@
 from LinkedList import LinkedList
 
 
+def LinkedListNumSumRev(linkedlist1, linkedlist2):
+    val = 0
+
+
+    temp = linkedlist1.head
+    len1 = 0
+    len2 = 0
+
+
+
+
+    while(temp != None):
+        len1 += 1
+        temp = temp.next
+
+
+    temp = linkedlist2.head
+
+    while(temp != None):
+        len2 += 1
+        temp = temp.next
+
+    temp1 = linkedlist1.head
+    temp2 = linkedlist2.head
+
+    counter = 0
+    while(temp1 != None):
+
+        val = val + temp1.value * 10 ** (len1-counter-1)
+
+        counter = counter+1
+
+        temp1 = temp1.next
+
+    counter = 0
+    while(temp2 != None):
+
+        val = val + temp2.value * 10 ** (len2-counter-1)
+
+        counter = counter+1
+
+        temp2 = temp2.next
+
+
+
+    return val
+
+
+
 def LinkedListNumSum(linkedlist1, linkedlist2):
     temp1 = linkedlist1.head
     temp2 = linkedlist2.head
@@ -42,8 +91,8 @@ linkedlist1.insertionListVals(vals1)
 linkedlist2.insertionListVals(vals2)
 
 res = LinkedListNumSum(linkedlist1,linkedlist2)
-
+res1 = LinkedListNumSumRev(linkedlist1, linkedlist2)
 print(res)
-
+print(res1)
 
 
